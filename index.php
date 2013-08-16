d00fe6e0e24c99cdfdca5860561ac508800e90f6
Robb Lewis <me@therobb.com>
2013-08-16 14:20:30 +0100

    Coming soon page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+	
+		<title>Just Delete Me | justdelete.me</title>
+		
+		<meta charset="UTF-8">
+		
+		
+		<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="shortcut icon" href="img/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		
+	</head>
+	
+	<body>
+	
+		<h1>Just<span class="red">Delete</span>.Me | Coming Soon</h1>
+
+	</body>
+	
+</html>
