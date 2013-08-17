709721e81542ed3ce4fa505a805ea81ab437088e
Robb Lewis <me@therobb.com>
2013-08-17 20:09:58 +0100

    Added no results message, fixed some element jumping, updated description

--- index.php
+++ index.php
@@ -44,19 +44,25 @@
 	<section class="main">		
 		<section class="sites" id="sites">
 
+			<p class="no-results">Can't find what you're looking for? <a href='#'>Help make justdelete.me better</a>.</p>
+
 		</section>
 	</section>
 	<section class="info-block">
 		<div class="info-container">
 			<h2>What is this?</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Many companies use <a href="http://darkpatterns.org/">dark pattern</a> techniques to make it difficult to find how to delete your account. JustDelete.me aims to be a repository of exactly what page you need to visit to delete your account.</p>
+
+			<p>The links above are colour-coded to indicate the difficulty level of account deletion:</p>
 			<ul>
 				<li><span class="green">Green:</span> Simple process</li>
-				<li><span class="yellow">Yellow:</span> Some steps involved</li>
-				<li><span class="red">Red:</span> Cannot be deleted without contacting customer services</li>
+				<li><span class="yellow">Yellow:</span> Some extra steps involved</li>
+				<li><span class="red">Red:</span> Cannot be fully deleted without contacting customer services</li>
 			</ul>
+
+			<p>Got a site you think should be added? <a href="http://github.com/rmlewisuk/justdelete.me">Fork the project GitHub</a>.</p>
 			<footer>
-				<span>Made by <a href="http://robblewis.me">Robb Lewis</a></span>
+				<span>Made by <a href="http://robblewis.me">Robb Lewis</a> | Designed by <a href="http://edpoole.me">Ed Poole</a> | Fork on <a href="http://github.com/rmlewisuk/justdelete.me">GitHub</a></span>
 			</footer>
 		</div>		
 	</section>	
