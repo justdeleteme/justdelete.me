0dffe953ecb3844813ef3b7816084e91793fdb95
Robb Lewis <me@therobb.com>
2013-08-17 17:41:03 +0100

    Instant search done. Like a boss

--- inc/search.js
+++ inc/search.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 
   $("input").keyup(function(){
 
-    var term = ".site a:not(:contains(" + $(this).val() + "))";
-    console.log(term);
+	var term = $(this).val();
     var $sites = $('.sites').children('section');
-    console.log($sites);
-    $sites.each(function(index, value) {
-    	console.log(index, value);
-    });
-    $(term).parent().css('display', 'none');
+
+    $sites.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(term);
+    }).hide();
   });
 });
