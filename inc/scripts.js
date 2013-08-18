95e18379989af35279e576ac839467e8f2811a31
Robb Lewis <me@therobb.com>
2013-08-18 12:40:38 +0100

    Commiting scripts.js

---
+++ inc/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){     
+
+	$.getJSON('sites.json', function(data) {
+			// Sort by name, asc
+			data.sort(function(a, b){
+			 var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+			 if (nameA < nameB)
+			  return -1 
+			 if (nameA > nameB)
+			  return 1
+			 return 0
+			});
+			for (i = 0; i < data.length; i++) {
+				if (data[i].notes) {
+					console.log(data[i].notes);
+					$('.sites').append("<section class='site "+data[i].difficulty+"'><a href='"+data[i].url+"'>"+data[i].name+"</a><div class='tooltip'><p>" + data[i].notes + "</p></div></section>");
+				}
+				else {
+					$('.sites').append("<section class='site "+data[i].difficulty+"'><a href='"+data[i].url+"'>"+data[i].name+"</a></section>");
+				}
+			}
+		});
+
+  $("input").keyup(function(){
+
+	if ($('.no-results').is(':visible')) {
+		$('.no-results').hide();
+	}
+
+	var term = $(this).val().toLowerCase();
+    var $sites = $('.sites').children('section');
+
+    $sites.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(term);
+    }).hide();
+
+    if ( ! $('.site').is(':visible')) {
+		console.log($('.site').is(':visible'));
+		$('.no-results').show();
+    }
+  });
+
+	$(".sites").on('mouseenter','.site',function () {
+		$(this).find(".tooltip").fadeIn();
+	});
+
+	$(".sites").on('mouseleave','.site',function () {
+	    $(this).find(".tooltip").fadeOut('fast');
+	});
+});
