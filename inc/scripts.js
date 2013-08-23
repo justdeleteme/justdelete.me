3f6e8bbf3b630c6f49c788a49f3af47c45b6df23
Dan Harper <intouch@danharper.me>
2013-08-23 10:16:46 +0100

    standardise indentation, and quotes

--- inc/scripts.js
+++ inc/scripts.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){     
+$(function(){     
 
-  $("input").keyup(function(){
+	$('input').keyup(function(){
 
-	if ($('.no-results').is(':visible')) {
-		$('.no-results').hide();
-	}
+		if ($('.no-results').is(':visible')) {
+			$('.no-results').hide();
+		}
 
-	var term = $(this).val().toLowerCase();
-    var $sites = $('.sites section');
+		var term = $(this).val().toLowerCase();
+		var $sites = $('.sites section');
 
-    $sites.show().filter(function() {
-        var text = $(this).find('a').text().replace(/\s+/g, ' ').toLowerCase();
-        return !~text.indexOf(term);
-    }).hide();
+		$sites.show().filter(function() {
+			var text = $(this).find('a').text().replace(/\s+/g, ' ').toLowerCase();
+			return !~text.indexOf(term);
+		}).hide();
 
-    if ( ! $('.site').is(':visible')) {
-		$('.no-results').show();
-    }
-  });
-
-	$(".site").hover(function () {
-		$(this).find(".tooltip").fadeIn();
-	}, function () {
-	    $(this).find(".tooltip").fadeOut('fast');
+		if ( ! $('.site').is(':visible')) {
+			$('.no-results').show();
+		}
 	});
+
+	$('.site').hover(function () {
+		$(this).find('.tooltip').fadeIn();
+	}, function () {
+		$(this).find('.tooltip').fadeOut('fast');
+	});
+
 });
