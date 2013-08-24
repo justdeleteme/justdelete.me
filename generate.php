8aba2fbdb097bd96c26f177e2311c0ae16ccba6a
Robb Lewis <me@therobb.com>
2013-08-24 14:18:44 +0100

    Moved FB like button script to scripts and load after page load

--- generate.php
+++ generate.php
@@ -51,13 +51,6 @@ usort($sites, function($a, $b) {
 
 	<!-- Facebook Like Button SDK -->
 	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
 
 	<header>
 		<h1>just<span>delete</span>.me</h1>
--- inc/scripts.js
+++ inc/scripts.js
@@ -51,3 +51,14 @@ $(function(){
 		}
 	});
 });
+
+// Load Facebook after page load
+window.onload = function() {
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+}
