90e9250723e2227e3872dcffdb736b6784905ef8
Lorenzo Rogai <lorenzo.rogai@hotmail.it>
2013-09-01 19:26:27 +0200

    Finished italian notes translation

--- dev.php
+++ dev.php
@@ -266,7 +266,7 @@
 	<div id="dropdown-1" class="dropdown dropdown-tip has-icons">
 	    <ul class="dropdown-menu">
 	    	<li class="en"><a href="/">English</a></li>
-	        <li class="it"><a href="it.html">Italiano <span class="beta">incompleto</span></a></li>
+	        <li class="it"><a href="it.html">Italiano</a></li>
 	    	<li class="de"><a href="de.html">Deutsch <span class="beta">unvollständig</span></a></li>
 	        <!-- <li class="fr"><a href="fr.html">French <span class="beta">incomplète</span></a></li> -->
 	        <!-- <li class="es"><a href="es.html">Spanish <span class="beta">incompleto</span></a></li> -->
