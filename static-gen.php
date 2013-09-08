745b091e5fdc078bd50b1bb8f7863dd9af54eb15
Andrey Knupp Vital <andreykvital@gmail.com>
2013-09-07 22:48:25 -0300

    Improving static site generation script.

--- static-gen.php
+++ static-gen.php
@@ -1,57 +1,24 @@
 <?php
-	
-	$langs = ['en', 'it', 'de', 'fr', 'ru', 'pt_br', 'cat', 'es'];
 
-	foreach ($langs as $language)
-	{
-		// start the output buffer
-		ob_start(); ?>
-		
-		<?php
+  $langs = [
+    'en' => 'English',
+    'it' => 'Italiano',
+    'de' => 'Deutsch',
+    'fr' => 'Français',
+    'ru' => 'Pусский',
+    'pt_br' => 'Português',
+    'cat' => 'Català',
+    'es' => 'Español'
+  ];
 
-		$lang = $language;
+  foreach ( $langs AS $lang => $full_name ) {
+      ob_start();
 
-		if ($lang == "en") {
-			$full_name = "English";
-		}
-		if ($lang == "it") {
-			$full_name = "Italiano";
-		}
-		if ($lang == "de") {
-			$full_name = "Deutsch";
-		}
-		if ($lang == "fr") {
-			$full_name = "Français";
-		}
-		if ($lang == "ru") {
-			$full_name = "Pусский";
-		}
-		if ($lang == "pt_br") {
-			$full_name = "Português";
-		}
-		if ($lang == "cat") {
-			$full_name = "Català";
-		}
-		if ($lang == "es") {
-			$full_name = "Español";
-		}
-		
-		include ('dev.php');
-
-		if ($language == "en")
-		{
-			$cachefile = "index.html";
-		}
-		else {
-			$cachefile = $language.".html";
-		}
-		// open the cache file "cache/home.html" for writing
-		$fp = fopen($cachefile, 'w'); 
-		// save the contents of output buffer to the file
-		fwrite($fp, ob_get_contents()); 
-		// close the file
-		fclose($fp); 
-		// Send the output to the browser
-		ob_end_flush();
-	}
+      include 'dev.php';
+      $cachefile = $language === 'en' ? 'index.html' : $language . '.html';
+      file_put_contents( $cachefile, ob_get_contents() );
+      
+      ob_end_flush();
+  }
+  
 ?>
