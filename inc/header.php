be91a75f57ccd89d011a9f700d45200eb770a61c
Robb Lewis <me@therobb.com>
2013-11-16 17:42:23 +0000

    Final updates to nav bar links

--- inc/header.php
+++ inc/header.php
@@ -49,10 +49,14 @@
 			<!-- begin language switcher -->
 			<span class="language-switch" href="#" data-dropdown="#dropdown-1" id="<?php echo $lang; ?>"><?php echo $full_name; ?></span>
 			<!-- end language switcher -->
+			<a href="#" class="info">About</a>
+			<a href="/fake-identity-generator" class="id-gen">Fake Identity Generator</a>
+			<a href="#" class="banner">Chrome Extension</a>
+		<?php else : ?>
+			<a href="/" class="home">Home</a>
+			<a href="/fake-identity-generator" class="id-gen current">Fake Identity Generator</a>
+			<a target="_blank" href="https://chrome.google.com/webstore/detail/justdeleteme/hfpofkfbabpbbmchmiekfnlcgaedbgcf">Chrome Extension</a>
 		<?php endif; ?>
-		<a href="#" class="info">About</a>
-		<a href="/fake-identity-generator" class="id-gen">Fake Identity Generator</a>
-		<a href="#" class="banner">Chrome Extension</a>
 		<a target="_blank" href="https://docs.google.com/a/therobb.com/forms/d/1mhr3vaTni5U8PvOdp_NvQ6vKBxNTmJTeKP3VWRuioCE/viewform">Submit a site</a>
 		<a target="_blank" href="http://github.com/rmlewisuk/justdelete.me">Fork on GitHub</a>
 	</nav>
