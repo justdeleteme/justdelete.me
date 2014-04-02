98a7224991c8c25544a3c2cf553fba8383c4ea77
Mojtahedi <mhm5000@gmail.com>
2014-04-03 01:34:03 +0430

    [rtl design] - final

--- inc/id-footer.php
+++ inc/id-footer.php
@@ -2,7 +2,7 @@
 		<div class="info-container">
 			<footer>
 				<span><?php echo $footer ?></span>
-				<div class="share-buttons" id="share buttons">
+				<div class="share-buttons<?php echo ($rtl) ? ' share-buttons-rtl' : '' ; ?>" id="share buttons">
 				<!-- Twitter -->
 					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://justdelete.me">Tweet</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
