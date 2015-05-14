9b5e3b8e67fdfb83985eb353f48d64e3bdc03fe1
Robb Lewis <me@therobb.com>
2015-05-14 09:16:41 +0100

    Update

--- dev.php
+++ dev.php
@@ -325,7 +325,7 @@
 	        <li class="id"><a href="id.html">Indonesia</a></li>
 	        <li class="ro"><a href="ro.html">Român</a></li>
 	        <li class="pl"><a href="pl.html">Polski</a></li>
-	        <li class="sk"><a href="sk.html">Slovakian</a></li>
+	        <li class="sk"><a href="sk.html">Slovenčina</a></li>
 	        <li class="dropdown-divider"></li>
 	    </ul>
 	</div>
