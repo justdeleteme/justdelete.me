3dd79a2b0f3e988f522b0adeec0dbf3ac1dbb519
Robb Lewis <me@therobb.com>
2015-08-31 12:06:41 +0100

    Add json lint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp      = require('gulp'),
     rename    = require('gulp-rename'),
     del       = require('del'),
     swig      = require('gulp-swig'),
-    data      = require('gulp-data');
+    data      = require('gulp-data')
+    jsonlint  = require('gulp-jsonlint');
     
 var translations = JSON.parse(fs.readFileSync('_trans/_config.json')),
     rtl = ['fa', 'ar'],
@@ -11,6 +12,12 @@ var translations = JSON.parse(fs.readFileSync('_trans/_config.json')),
     trans,
     notes;
 
+gulp.task('jsonlint', function() {
+    gulp.src('sites.json')
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter());
+});
+    
 gulp.task('clean', function(callback) {
     return del(['site/*.html'], callback);
 });
@@ -46,4 +53,4 @@ gulp.task('translate', ['clean'], function() {
   
 });
 
-gulp.task('default', ['translate']);
+gulp.task('default', ['jsonlint', 'translate']);
